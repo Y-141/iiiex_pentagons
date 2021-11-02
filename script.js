2ec82616118a47f9fddeef998b186907e49f2126
Y-141 <yk4215s.no@gmail.com>
2021-11-03 05:17:18 +0900

    add: switch button, title box, movie

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $(function() {
-  $('#content_fg').click(function(){
-    $(this).css('z-index', '1');
-    $('#content_bg').css('z-index', '2');
+  
+  $('#play_button').click(function(){
+    $('#audio').get(0).play();
+    $('#title_box').fadeOut();
+    $('#button').fadeIn();
+  })
+  
+  $('#button').click(function(){
+    if($('#content_fg').css('z-index') == '2'){
+      $('#content_fg').css('z-index', '1');
+      $('#content_bg').css('z-index', '2');
+    }else{
+      $('#content_bg').css('z-index', '1');
+      $('#content_fg').css('z-index', '2');
+    }
   });
-  $('#content_bg').click(function(){
-    $(this).css('z-index', '1');
-    $('#content_fg').css('z-index', '2');
-  });
+
 });
