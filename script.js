417915e12552e88c9387da9cbe52113e49ea8639
Y-141 <yk4215s.no@gmail.com>
2021-11-19 09:36:14 +0900

    fix: replay button position, fix: turtle_button appears when animals changed

--- script.js
+++ script.js
@@ -144,30 +144,44 @@ $(function() {
     if ($('#bg_video').get(0).currentTime >= 9.7 && $('#bg_video').get(0).currentTime <= 10.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 19.7 && $('#bg_video').get(0).currentTime <= 20.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 29.7 && $('#bg_video').get(0).currentTime <= 30.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 39.7 && $('#bg_video').get(0).currentTime <= 40.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 49.7 && $('#bg_video').get(0).currentTime <= 50.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 59.7 && $('#bg_video').get(0).currentTime <= 60.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
     else if ($('#bg_video').get(0).currentTime >= 69.7 && $('#bg_video').get(0).currentTime <= 70.5){
       $('#content_bg').css('z-index', '1');
       $('#content_fg').css('z-index', '2');
+      $('#switch_button').children('img').attr('src', 'images/icon_turtle.png');
+      $('#switch_button').children('img').attr('id', 'icon_turtle');
     }
   });
 
